38457d9a2b349636bb8521f3a745f2967e25dc07
Chuck Lee <sapronlee@gmail.com>
2011-02-10 11:55:43 +0800

    初始化项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chuckblog::Application.config.secret_token = '26f02bf90b7498d6d2304c024096441af353fcc6f6e5121cb632bb75b5b92327564fea2458a656470650a8b861a6562c36498259556a21f4ad09ce87bee00ee9'
