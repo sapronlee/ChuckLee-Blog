655656f5ae1938601cd52a566e2e44940e48e6c6
Chuck Lee <sapronlee@gmail.com>
2011-02-10 19:12:27 +0800

    初始化

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,6 @@
+# encoding : utf-8
+module LayoutHelper
+  def title(page_title)
+    content_for(:title) { "ChuckBlog | #{h(page_title.to_s)}" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Chuckblog::Application.routes.draw do
+  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'register' }
+  
+  root :to => 'home#index'
+  
+  namespace :admin do
+    root :to => 'home#index'
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
