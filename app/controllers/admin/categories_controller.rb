3fb97eac2aa2e796e3baf1f9cb94902032f78a8a
Chuck Lee <sapronlee@gmail.com>
2011-02-21 16:09:31 +0800

    添加后台category添加，删除，更新等功能

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,43 @@
+# coding : utf-8
 class Admin::CategoriesController < Admin::ApplicationController
   def index
+    @categories = initialize_grid(Category,
     
+                                  :per_page => Setting.page_size)
+  end
+  
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to(admin_categories_path, :notice => "新的分类创建成功！")
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(params[:category])
+      redirect_to(admin_categories_path, :notice => "分类修改成功！")
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @category = Category.find(params[:id])
+    if @category.destroy
+      redirect_to(admin_categories_path, :notice => "分类删除成功！")
+    else
+      redirect_to(admin_categories_path, :notice => "分类删除失败！")
+    end
   end
 end
--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,2 +1,8 @@
 module Admin::ApplicationHelper
+  # 去除区域里面的内容的换行标记  
+  def spaceless(&block)
+    data = with_output_buffer(&block)
+    data = data.gsub(/>\s+</,"><")
+    data
+  end
 end
