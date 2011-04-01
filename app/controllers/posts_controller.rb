90621294c86e6ea65c3c7152903b76ad5b6f5beb
Chuck Lee <sapronlee@gmail.com>
2011-04-01 23:43:27 +0800

    添加标签云

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,13 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find(params[:id], :include => [:category])    
-  end
+  end  
   
   private
   def fill_sidebar_data
     @categories = Category.find(:all)
     @recent_posts = Post.recent.find(:all, :limit => 5)
     @recent_comments = Comment.recent.find(:all, :limit => 5, :include => [:commentable])
+    @tags = Post.tag_counts_on(:tags)
   end
 end
