f81693d8d1e4e9af7316ce7857bbc86d8d5b625c
Chuck Lee <sapronlee@gmail.com>
2011-04-03 02:42:32 +0800

    add sidebar cached

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   theme :theme_resolver
   
+  
   private
+  
   def theme_resolver
     "default"
+  end
+  
+  def fill_sidebar_data
+    @categories = Category.order("id desc")
+    @recent_posts = Post.recent.limit(5)
+    @recent_comments = Comment.recent.limit(5)    
+    @tags = Post.tag_counts_on(:tags)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,18 +3,11 @@ class PostsController < ApplicationController
   before_filter :fill_sidebar_data, :only => [:index, :show]
   
   def index
-    @posts = Post.recent.paginate(:page => params[:page], :per_page => Setting.page_size, :include => [:category])    
+    @posts = Post.recent.includes(:category).paginate(:page => params[:page], :per_page => Setting.page_size)    
   end
   
   def show
     @post = Post.find(params[:id], :include => [:category])    
   end  
   
-  private
-  def fill_sidebar_data
-    @categories = Category.find(:all)
-    @recent_posts = Post.recent.find(:all, :limit => 5)
-    @recent_comments = Comment.recent.find(:all, :limit => 5, :include => [:commentable])
-    @tags = Post.tag_counts_on(:tags)
-  end
 end
