3476629307aae31f0b9fdbf473e1c092828c2d4f
Chuck Lee <sapronlee@gmail.com>
2011-04-05 03:19:58 +0800

    autoload lib file

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,2 +1,7 @@
 # 配置文件载入
 APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")[Rails.env]
+
+# 自动加载 config.autoload_paths 的文件
+Chuckblog::Application.config.autoload_paths.each do |c|
+  Dir["#{c}/*.rb"].each { |f| require f }
+end
