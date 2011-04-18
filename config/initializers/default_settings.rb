9e08ab7ae96afae9289431dceadb4ce9d95b7f35
Chuck Lee <sapronlee@gmail.com>
2011-04-18 17:36:26 +0800

    后台设置页面添加几个内容，增加设置页面的双击扩大/缩小文本框

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,8 +1,24 @@
 # coding: utf-8
 
-#默认设置
+# 默认设置
+
+# 站点名称
 Setting.defaults[:site_name] = "ChuckLeeBlog"
+# 网站名称
 Setting.defaults[:app_name] = "ChuckBlog"
+# 分页数
 Setting.defaults[:page_size] = 20
+# 后台分页数
+Setting.defaults[:admin_page_size] = 20
+# 主题
 Setting.defaults[:theme] = "vulcan"
+# 第三方统计代码
 Setting.defaults[:statistics_code] = ""
+# 网站关键字
+Setting.defaults[:key_words] = ""
+# 网站描述
+Setting.defaults[:description] = ""
+# 网站备案信息
+Setting.defaults[:icp] = ""
+# 是否显示网站备案信息
+Setting.defaults[:is_show_icp] = 0
